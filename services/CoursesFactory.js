442bb29c5e085855a4d2d034b8073b50c1e4f20f
Virginie Trubiano & Bojana Skarich <student@epicodus.com>
2015-04-22 15:44:25 -0700

    Add alternative code for when you don't create a UtilitiesFactory.

--- services/CoursesFactory.js
+++ services/CoursesFactory.js
@@ -1,9 +1,19 @@
 courseRoster.factory('CoursesFactory', function CoursesFactory() {
-    var factory = {} ;
+    var factory = {};
     factory.courses = [];
     factory.addCourse = function() {
         factory.courses.push({ name: factory.courseName, id: factory.courses.length + 1, students: [] });
         factory.courseName = null;
     };
+
+//This is alternative code if you don't want to create another factory (UtilitiesFactory) in order to find course by id
+    // factory.findById = function(id) {
+    //     for (var i= 0; i < factory.courses.length; i++) {
+    //         if (factory.courses[i].id == id) {
+    //             return factory.courses[i];
+    //         }
+    //     }
+    //     return null;
+    // };
     return factory;
 });
--- services/UtilitiesFactory.js
+++ services/UtilitiesFactory.js
@@ -1,12 +1,13 @@
 courseRoster.factory('UtilitiesFactory', function() {
-    var factory= {};
-    factory.findById = function(collection, id) {
-        for (var i= 0; i < collection.length; i++) {
-            if (collection[i].id == id) {
-                return collection[i];
+    var factory = {
+        findById: function(collection, id) {
+            for (var i= 0; i < collection.length; i++) {
+                if (collection[i].id == id) {
+                    return collection[i];
+                }
             }
+            return null;
         }
-        return null;
-    };
+    }
     return factory;
 });
